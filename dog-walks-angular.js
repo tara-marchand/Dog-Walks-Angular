94310e4d781bfb4d39cc4d990181c9e947e57cd2
Tara Marchand <tara@mac.com>
2013-05-12 22:29:07 -0700

    Got list/detail views and switching set up

--- dog-walks-angular.js
+++ dog-walks-angular.js
@@ -1,25 +1,33 @@
 (function() {
 
-	var dogWalks = angular.module("dogWalks", ["firebase"]);
+	var dogWalks = angular.module("dogWalks", ["firebase"])
+		.config(["$routeProvider", function($routeProvider) {
+			$routeProvider.when("/dogs", { templateUrl: "views/dog-list.html" })
+				.when("/dogs/:dogName", { templateUrl: "views/dog-detail.html" })
+				.otherwise({ redirectTo: "/dogs" });
+			}
+		]);
 
-	dogWalks.controller("dogsController", function ($scope, angularFire) {
-
+	dogWalks.controller("dogWalksController", function ($scope, $routeParams, angularFire, $location) {
 		var dogsUrl = "https://dog-walks-angular.firebaseio.com/dogs";
 		var promise = angularFire(dogsUrl, $scope, "dogs");
 
 		promise.then(function() {
+			$scope.dogName = $routeParams.dogName;
+
 			$scope.addDog = function() {
 				$scope.dogs.push({
 					name: $scope.newDog.name
+					breed: $scope.newDog.breed
+					age: $scope.newDog.age
 				});
 			};
 
 			$scope.removeDog = function(dog) {
 				$scope.dogs.splice($scope.dogs.indexOf(dog), 1);
 			};
-
 			$scope.$watch("dogs");
 		});
-
 	});
+
 })();
